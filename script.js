5dae8021e1517febc531f340cf0698b8c9ecbfb3
Nataliia <d_tasha@ukr.net>
2022-02-19 22:37:39 +0200

    added get function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 class Employee {
-  static numberOfVacation = 18;
+  static NUMBER_OF_VACATION = 18;
+  numberOfWorkDays = 365 - Employee.NUMBER_OF_VACATION;
+
   constructor(firstName, lastName, age, jobPosition, salary) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -12,9 +14,7 @@ class Employee {
     console.log(this.firstName, this.lastName);
   }
   countSalary() {
-    const salaryFullYear =
-      ((this.salary * 12) / 365) * (365 - this.numberOfVacation);
-    return salaryFullYear;
+    return Math.round(((this.salary * 12) / 365) * this.numberOfWorkDays);
   }
 }
 const Employee_1 = new Employee("Ivan", "Ivanov", 25, "junior", 500);
@@ -22,3 +22,4 @@ const Employee_2 = new Employee("Petr", "Petrov", 35, "senior", 6000);
 Employee_1.fullName;
 Employee_2.fullName;
 console.log(Employee_1.countSalary());
+console.log(Employee_2.countSalary());
